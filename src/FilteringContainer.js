682224dafc390e71f077a39c993925fb91eb9c78
Patryk Mazurkiewicz <patrykiewicz@gmail.com>
2020-03-26 08:50:21 +0100

    filterNodeAction

--- src/FilteringContainer.js
+++ src/FilteringContainer.js
@@ -43,14 +43,14 @@ export default class FilteringContainer extends React.Component {
     this.setState(ps => ({filterTerm: ps.filterText}));
   }
 
-  handleFilterTextChange = (e, cb) => {
+  handleFilterTextChange = e => {
     const filterText = e.target.value;
+
+    this.props.filterNodeAction && this.props.filterNodeAction();
 
     this.setState({filterText});
 
     this.setFilterTerm();
-
-    cb && cb();
   };
 
   render() {
@@ -64,7 +64,6 @@ export default class FilteringContainer extends React.Component {
       onSelectedGroupChange,
       renderSearch,
       customNodeFilter,
-      postFilterNodeAction,
     } = this.props;
 
     const relevantNodes =
@@ -73,9 +72,7 @@ export default class FilteringContainer extends React.Component {
         : {nodes, nodeParentMappings: {}};
 
     const {nodes: filteredNodes, nodeParentMappings} = filterTerm
-      ? postFilterNodeAction
-        ? postFilterNodeAction(filterNodes(nameMatchesSearchTerm(filterTerm), relevantNodes.nodes))
-        : filterNodes(nameMatchesSearchTerm(filterTerm), relevantNodes.nodes)
+      ? filterNodes(nameMatchesSearchTerm(filterTerm), relevantNodes.nodes)
       : relevantNodes;
 
     return (
@@ -105,6 +102,5 @@ FilteringContainer.propTypes = {
   selectedGroup: PropTypes.string,
   groupRenderer: PropTypes.func,
   renderSearch: PropTypes.func,
-  postFilterNodeAction: PropTypes.func,
-  customNodeFilter: PropTypes.func,
+  filterNodeAction: PropTypes.func,
 };
